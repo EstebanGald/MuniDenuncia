bd26bdaa9f9f125dcfa783436e000e6f585c1248
sebabenn1234 <sebastian.meneses@sansano.usm.cl>
2023-10-18 15:20:21 -0300

    Update Initial File
    Actualize los archivos principalmente utilizados dentro de la app

---
+++ App/src/app.jsx
@@ -0,0 +1,7 @@
+//Importa las funcionalidades de js
+
+function App() {
+
+}
+
+export default App
---
+++ App/src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+import App from "./app";
+import "./stylesheet/index.css"
+import { BrowserRouter } from "react-router-dom";
+
+ReactDom.createRoot(document.getElementById("root")).render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
